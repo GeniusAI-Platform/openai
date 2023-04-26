79e78c18410c65ccafde7a76e9bb3f0107a9501e
Javad <ja7ad@live.com>
2023-04-26 12:13:22 +0330

    add: create image, image variation and edit image

---
+++ image.go
@@ -0,0 +1,186 @@
+package openai
+
+import (
+	"bytes"
+	"context"
+	"github.com/GoFarsi/openai/client"
+	"github.com/GoFarsi/openai/entity"
+	"github.com/GoFarsi/openai/utils"
+	"net/http"
+	"strconv"
+)
+
+const (
+	createImageEndpoint          = "/images/generations"
+	editImageEndpoint            = "/images/edits"
+	createImageVariationEndpoint = "/images/variations"
+)
+
+type Image struct {
+	client client.Transporter
+}
+
+func NewImage(client client.Transporter) *Image {
+	return &Image{
+		client: client,
+	}
+}
+
+// CreateImage Creates an image given a prompt
+func (i *Image) CreateImage(ctx context.Context, req entity.ImageRequest) (*entity.ImageResponse, error) {
+	if err := i.client.GetValidator().Struct(req); err != nil {
+		return nil, err
+	}
+
+	resp, err := i.client.Post(ctx, &client.APIConfig{Path: createImageEndpoint}, req)
+	if err != nil {
+		return nil, err
+	}
+
+	response := new(entity.ImageResponse)
+	errResp := new(entity.ErrorResponse)
+
+	if resp.GetHttpResponse().StatusCode != http.StatusOK {
+		if err = resp.GetJSON(errResp); err != nil {
+			return nil, err
+		}
+		errResp.HttpCode = resp.GetHttpResponse().StatusCode
+		return nil, errResp
+	}
+
+	if err = resp.GetJSON(response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+// ImageEdit Creates an edited or extended image given an original image and a prompt
+func (i *Image) ImageEdit(ctx context.Context, req entity.ImageEditRequest) (*entity.ImageResponse, error) {
+	if err := i.client.GetValidator().Struct(req); err != nil {
+		return nil, err
+	}
+
+	body := new(bytes.Buffer)
+	fb := utils.NewFormBuilder(body)
+
+	if err := fb.CreateFormFile("image", req.Image); err != nil {
+		return nil, err
+	}
+
+	if req.Mask != nil {
+		if err := fb.CreateFormFile("mask", req.Mask); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := fb.WriteField("prompt", req.Prompt); err != nil {
+		return nil, err
+	}
+
+	if req.N == 0 {
+		req.N = 1
+	}
+
+	if err := fb.WriteField("n", strconv.Itoa(req.N)); err != nil {
+		return nil, err
+	}
+
+	if err := fb.WriteField("size", req.Size.String()); err != nil {
+		return nil, err
+	}
+
+	if err := fb.WriteField("response_format", req.ResponseFormat.String()); err != nil {
+		return nil, err
+	}
+
+	if err := fb.WriteField("user", req.User); err != nil {
+		return nil, err
+	}
+
+	if err := fb.Close(); err != nil {
+		return nil, err
+	}
+
+	resp, err := i.client.PostFile(ctx, &client.APIConfig{Path: editImageEndpoint}, body, fb.FormDataContentType())
+	if err != nil {
+		return nil, err
+	}
+
+	response := new(entity.ImageResponse)
+	errResp := new(entity.ErrorResponse)
+
+	if resp.GetHttpResponse().StatusCode != http.StatusOK {
+		if err = resp.GetJSON(errResp); err != nil {
+			return nil, err
+		}
+		errResp.HttpCode = resp.GetHttpResponse().StatusCode
+		return nil, errResp
+	}
+
+	if err = resp.GetJSON(response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+// CreateImageVariation Creates a variation of a given image
+func (i *Image) CreateImageVariation(ctx context.Context, req entity.ImageVariationRequest) (*entity.ImageResponse, error) {
+	if err := i.client.GetValidator().Struct(req); err != nil {
+		return nil, err
+	}
+
+	body := new(bytes.Buffer)
+	fb := utils.NewFormBuilder(body)
+
+	if err := fb.CreateFormFile("image", req.Image); err != nil {
+		return nil, err
+	}
+
+	if req.N == 0 {
+		req.N = 1
+	}
+
+	if err := fb.WriteField("n", strconv.Itoa(req.N)); err != nil {
+		return nil, err
+	}
+
+	if err := fb.WriteField("size", req.Size.String()); err != nil {
+		return nil, err
+	}
+
+	if err := fb.WriteField("response_format", req.ResponseFormat.String()); err != nil {
+		return nil, err
+	}
+
+	if err := fb.WriteField("user", req.User); err != nil {
+		return nil, err
+	}
+
+	if err := fb.Close(); err != nil {
+		return nil, err
+	}
+
+	resp, err := i.client.PostFile(ctx, &client.APIConfig{Path: createImageVariationEndpoint}, body, fb.FormDataContentType())
+	if err != nil {
+		return nil, err
+	}
+
+	response := new(entity.ImageResponse)
+	errResp := new(entity.ErrorResponse)
+
+	if resp.GetHttpResponse().StatusCode != http.StatusOK {
+		if err = resp.GetJSON(errResp); err != nil {
+			return nil, err
+		}
+		errResp.HttpCode = resp.GetHttpResponse().StatusCode
+		return nil, errResp
+	}
+
+	if err = resp.GetJSON(response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
