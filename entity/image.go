a736bced2419d3e7a348cab0877205a279ee3c10
Javad <ja7ad@live.com>
2023-04-26 12:14:30 +0330

    fix: change size type to ImageSize type

--- entity/image.go
+++ entity/image.go
@@ -40,7 +40,7 @@ type ImageVariationRequest struct {
 	// N The number of images to generate. Must be between 1 and 10
 	N int `json:"n,omitempty"`
 	// Size The size of the generated images. Must be one of ImageSize256x256, ImageSize512x512, or ImageSize1024x1024
-	Size string `json:"size,omitempty"`
+	Size ImageSize `json:"size,omitempty"`
 	// ResponseFormat The format in which the generated images are returned. Must be one of ImageResponseFormatURL or ImageResponseFormatB64JSON
 	ResponseFormat ImageResponseFormat `json:"response_format,omitempty"`
 	// User A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse
